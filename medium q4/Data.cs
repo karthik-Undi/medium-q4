015f33d599c1eb7f6bfba6b3608b8624d34d2baa
karthik <karthik.venkata@kanini.com>
2021-01-27 12:41:19 +0530

    Add project files.

---
+++ medium q4/Data.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium_q1
+{
+    class Data
+    {
+        public static List<Employee> Employees = new List<Employee>();
+        public static void PrintEmployee()
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("\nPlease enter the employee ID\n");
+            int key = Convert.ToInt32(Console.ReadLine());
+            var age = (from Employee in Employees where Employee.Id == key select Employee.Age);
+            int y=0;
+            foreach(var i in age)
+                y = Convert.ToInt32(i);
+
+            var emp = (from Employee in Employees where Employee.Age >= y select Employee);
+            Console.WriteLine("\nEmployees who are older than employee with ID {0} ({1} years old ) are \n",key,y);
+
+            foreach (var i in emp)
+            {
+
+                Console.WriteLine(i+"\n");
+            }
+
+        }
+        public static void SortAndPrintEmployees()
+        {
+
+            Employees.RemoveAt(Employees.Count - 1);
+            Data.Employees.Sort();
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("\nEmployee list after sorting \n");
+            Data.Employees.ForEach(employee => Console.WriteLine(employee+"\n"));
+            
+
+
+
+        }
+
+    }
+}
---
+++ medium q4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium_q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data.Employees.Add(new Employee());
+            for (int i = 0; ; i++)
+            {
+                int res = Data.Employees.ElementAt(i).TakeEmployeeDetailsFromUser();
+                if (res == 1)
+                {
+                    Console.WriteLine("Duplicate Employee ID !!!!");
+                    i--;
+
+                }
+                else
+                {
+                    Console.WriteLine("To continue entering employee details enter 1, to exit enter 0");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 1)
+                        continue;
+                    else if (choice == 0)
+                    {
+                        Data.SortAndPrintEmployees();
+                        Data.PrintEmployee();
+                        break;
+                    }
+
+                    else
+                        Console.WriteLine("Wrong input ");
+
+                }
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
